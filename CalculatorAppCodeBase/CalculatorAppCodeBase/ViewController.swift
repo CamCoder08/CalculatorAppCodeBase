193bc0f27e47508f5ab1f6e7a22f03075b5d0d00
CamCoder08 <y30281@gmail.com>
2025-03-29 14:37:12 +0900

    feat: 수평 스택뷰 구성 및 버튼 함수 생성
    - makeButton(title:) 버튼 생성 함수 분리
    - makeHorizontalStackView() 함수에서 7, 8, 9, + 버튼을 호출 후 수평으로 배치 (StackView)
    - 스타일 및 오토레이아웃 추가

--- CalculatorAppCodeBase/CalculatorAppCodeBase/ViewController.swift
+++ CalculatorAppCodeBase/CalculatorAppCodeBase/ViewController.swift
@@ -14,13 +14,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        configure()
+        configure() // 수식 표시
+        makeHorizontalStackView() // 스택뷰
     }
 
     private func configure() {
         view.backgroundColor = .black
 
-        resultLabel.text = "\(resultLabel)" // 스트링 인터폴레이션으로 정수 담기
+        resultLabel.text = "\(resultNumber)" // 스트링 인터폴레이션으로 정수 담기
         resultLabel.textColor = .white
         resultLabel.textAlignment = .right
         resultLabel.font = .boldSystemFont(ofSize: 60)
@@ -34,7 +35,49 @@ class ViewController: UIViewController {
             resultLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 30),
             resultLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -30),
             resultLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 200)
+                                    ])
+    }
+
+    private func makeButton(title: String) -> UIButton { // 버튼 생성 함수
+        //            for title in arrTitle    {
+        let button = UIButton()
+        button.setTitle(title, for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 30)
+        button.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
+
+        NSLayoutConstraint.activate([
+            button.heightAnchor.constraint(equalToConstant: 80),
+            button.widthAnchor.constraint(equalToConstant: 80)
+        ])
+
+        return button
+
+    }
+
+    private func makeHorizontalStackView() { // 수평 생성 함수
+
+        let button7 = makeButton(title: "7")
+        var button8 = makeButton(title: "8")
+        var button9 = makeButton(title: "9")
+        var plusbButton = makeButton(title: "+")
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.backgroundColor = .black
+        stackView.spacing = 10
+        stackView.distribution = .fillEqually
+        stackView.addArrangedSubview(button7)
+        stackView.addArrangedSubview(button8)
+        stackView.addArrangedSubview(button9)
+        stackView.addArrangedSubview(plusbButton)
+
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+
+        view.addSubview(stackView)
+
+        NSLayoutConstraint.activate([
+            stackView.heightAnchor.constraint(equalToConstant: 80),
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
     }
 }
-
