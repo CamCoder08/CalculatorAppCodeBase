22f165b1df41b5158d1dfb621079f8d67ba2a041
CamCoder08 <y30281@gmail.com>
2025-04-01 15:11:51 +0900

    feat: 연산자 및 AC 버튼에 오렌지색 배경 적용
    버튼 생성 로직에 조건문을 추가하여 +, -, *, /, AC 버튼은 오렌지색 배경이 적용되도록 구현.

--- CalculatorAppCodeBase/CalculatorAppCodeBase/ViewController.swift
+++ CalculatorAppCodeBase/CalculatorAppCodeBase/ViewController.swift
@@ -21,9 +21,6 @@ class ViewController: UIViewController {
                                                        ["AC", "0", "=", "/"]])
         let verticalStackView: [UIStackView] = makeHorizontalStackView(buttons)
         makeVerticalStackView(verticalStackView)
-
-        //        makeHorizontalStackView(buttons) // 함수를 사용하지 않아서 노란색 경고 (빌드 문제 없음)
-        //        makeVerticalStackView
     }
 
     private func configure() {
@@ -43,34 +40,46 @@ class ViewController: UIViewController {
             resultLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
             resultLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
             resultLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 200)
-                                    ])
-
+        ])
     }
     // 버튼 생성 함수
-    func makeButton(titles: [[String]]) -> [[UIButton]] {
-        var arrButton: [[UIButton]] = []
+    func makeButton(titles: [[String]]) -> [[UIButton]] { // 스트링 2차원 배열로
+        var arrButtons: [[UIButton]] = []
         for arrTitles in titles {
-            var arrButtons: [UIButton] = []
+            var arrButton: [UIButton] = []
             for title in arrTitles { // 버튼 생성 반복문
                 let button: UIButton = UIButton() // 버튼
-                button.setTitle(title, for: .normal)
-                button.titleLabel?.font = .boldSystemFont(ofSize: 30)
-                button.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
+                // for문으로 버튼을 생성할 때 if 조건문을 활용하여 +, -, *, /, AC 값이 들어오면 버튼 배경색을 orange로 설정
+                if title == "+" || title == "-" || title == "*" || title == "/" || title == "AC" {
+                    button.setTitle(title, for: .normal)
+                    button.backgroundColor = .orange
+                    button.titleLabel?.font = .boldSystemFont(ofSize: 30)
+                    button.translatesAutoresizingMaskIntoConstraints = false
 
-                button.translatesAutoresizingMaskIntoConstraints = false
+                    NSLayoutConstraint.activate([
+                        button.heightAnchor.constraint(equalToConstant: 80),
+                        button.widthAnchor.constraint(equalToConstant: 80)
+                    ])
+                    arrButton.append(button) // 버튼 배열에 추가
+                } else {
+                    button.setTitle(title, for: .normal)
+                    button.titleLabel?.font = .boldSystemFont(ofSize: 30)
+                    button.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
 
-                NSLayoutConstraint.activate([
-                    button.heightAnchor.constraint(equalToConstant: 80),
-                    button.widthAnchor.constraint(equalToConstant: 80)
-                ])
-                arrButtons.append(button) // 생성된 버튼 배열에 추가
+                    button.translatesAutoresizingMaskIntoConstraints = false
+
+                    NSLayoutConstraint.activate([
+                        button.heightAnchor.constraint(equalToConstant: 80),
+                        button.widthAnchor.constraint(equalToConstant: 80)
+                    ])
+                    arrButton.append(button) // 버튼 배열에 추가
+                }
             }
-            arrButton.append(arrButtons)
+            arrButtons.append(arrButton) // 버튼 배열을 2차원 버튼 배열에 추가
         }
-        return arrButton
+        return arrButtons
     }
-
-    // 수평 StackView 생성 함수 (수평 스택뷰 4개 생성..?)
+    // 수평 StackView 생성 함수
     func makeHorizontalStackView(_ buttonArray: [[UIButton]]) -> [UIStackView] {
         var horizontalStackView: [UIStackView] = []
 
@@ -83,13 +92,11 @@ class ViewController: UIViewController {
             stackView.translatesAutoresizingMaskIntoConstraints = false
 
             for arrbutton in horizontalArrButton {
-                stackView.addArrangedSubview(arrbutton)
+                stackView.addArrangedSubview(arrbutton) // 버튼 4개가 1개의 스택뷰로
             }
-
-            horizontalStackView.append(stackView)
+            horizontalStackView.append(stackView) // 1개의 스택뷰를 4개의 스택뷰로
         }
         return horizontalStackView
-
     }
 
     func makeVerticalStackView(_ numberPad: [UIStackView]) {
@@ -99,19 +106,19 @@ class ViewController: UIViewController {
         verticalStackView.spacing = 10
         verticalStackView.distribution = .fillEqually
 
-        verticalStackView.translatesAutoresizingMaskIntoConstraints = false
-
         for arrStackView in numberPad {
             verticalStackView.addArrangedSubview(arrStackView)
-        }
+        } // 4개의 스택뷰 2차원배열을 각 하나씩 꺼내서 세로 스택뷰에 넣어주기
 
         view.addSubview(verticalStackView)
 
+        verticalStackView.translatesAutoresizingMaskIntoConstraints = false
+
         NSLayoutConstraint.activate([
-                    verticalStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
-                    verticalStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
-                    verticalStackView.topAnchor.constraint(equalTo: resultLabel.bottomAnchor, constant: 60),
-                    verticalStackView.bottomAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
-                ])
+            verticalStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            verticalStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            verticalStackView.topAnchor.constraint(equalTo: resultLabel.bottomAnchor, constant: 60),
+            verticalStackView.bottomAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20)
+        ])
     }
 }
